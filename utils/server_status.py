360a8a4752887ff3cd1b1705652fe38918f5e965
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2023-07-29 15:20:34 -0400

    server satus file

---
+++ utils/server_status.py
@@ -0,0 +1,26 @@
+"""
+file_name = server_status.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/29/2023
+Description: A file used to check the server status
+Edit Log: 
+07/29/2023 
+    - Moved over file from original tool receiver
+"""
+
+import os
+import requests  # pylint: disable=import-error
+
+
+def server_status() -> bool:
+    """
+    Check if the server is running correctly, by pinging it
+
+    Returns:
+        bool: Returns true if server is running, false if not
+    """
+
+    url: str = os.getenv("BASE_URL")
+    header: requests.Response = requests.head(url)
+
+    return header.status_code == 200
