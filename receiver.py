e822f478811e1ed4bf6edf3da82ceb8da0f60856
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2023-07-30 21:23:09 -0400

    events receiver

--- receiver.py
+++ receiver.py
@@ -11,25 +11,47 @@ Edit Log:
 """
 
 from argparse import ArgumentParser, Namespace
+from importlib import import_module
 from typing import Dict, Callable
 
-from server_requests.weather import get_weather
 from utils.timer import Timer
+
+from server_requests.events import get_default_events
+from server_requests.weather import get_weather
 
 PARSER: ArgumentParser = ArgumentParser()
 
 # ARGUMENTS
+PARSER.add_argument(
+    "-df",
+    "--default",
+    help="Filter events by default options: today, weekly, month or year",
+    type=str,
+)
+
 PARSER.add_argument("-w", "--weather", help="Get todays weather", action="store_true")
 
-ARG_TO_FUNCTION: Dict[str, Callable] = {
-    "weather": get_weather,
+ARG_TO_FUNCTION: Dict[str, Dict[str, any]] = {
+    "weather": {
+        "function": get_weather,
+        "pass_args": False
+    },
+    "default": {
+        "function": get_default_events,
+        "pass_args": True
+    }
 }
 
 if __name__ == "__main__":
     args: Namespace = PARSER.parse_args()
-
+    
     for arg in vars(args):
         if getattr(args, arg) and ARG_TO_FUNCTION.get(arg):
-            ARG_TO_FUNCTION[arg]()
+            arg_method: Callable = ARG_TO_FUNCTION[arg]["function"]
+
+            if ARG_TO_FUNCTION[arg]["pass_args"]:
+                arg_method(args)
+            else:
+                arg_method()
 
     Timer().print_and_format_time("Total Time:")
