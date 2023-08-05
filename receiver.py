e0721f56924f02cafb6356fb0721d2f71241974e
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2023-08-04 22:29:27 -0400

    logs argument

--- receiver.py
+++ receiver.py
@@ -8,6 +8,8 @@ Edit Log:
     - Created new receiver file
     - Added weather argument
     - Added total execution time
+08/04/2023
+    - Added logs argument
 """
 
 from argparse import ArgumentParser, Namespace
@@ -18,9 +20,11 @@ from utils.timer import Timer
 from server_requests.diagnostics import get_diagnostics
 from server_requests.emails import get_default_emails
 from server_requests.events import get_default_events
+from server_requests.logs import get_logs
 from server_requests.weather import get_weather
 
 PARSER: ArgumentParser = ArgumentParser()
+
 
 # ARGUMENTS
 PARSER.add_argument(
@@ -41,14 +45,20 @@ PARSER.add_argument(
     "-diagnostics", "--diagnostics", help="Show endpoint diagnostics", type=str
 )
 
+PARSER.add_argument("-gl", "--getlogs", help="Get today's Logs", action="store_true")
+
 PARSER.add_argument("-w", "--weather", help="Get todays weather", action="store_true")
 
+
+# FUNCTION MAPPING
 ARG_TO_FUNCTION: Dict[str, Dict[str, any]] = {
     "default": {"function": get_default_events, "pass_args": True},
     "dgmail": {"function": get_default_emails, "pass_args": False},
     "diagnostics": {"function": get_diagnostics, "pass_args": True},
+    "getlogs": {"function": get_logs, "pass_args": False},
     "weather": {"function": get_weather, "pass_args": False},
 }
+
 
 if __name__ == "__main__":
     args: Namespace = PARSER.parse_args()
