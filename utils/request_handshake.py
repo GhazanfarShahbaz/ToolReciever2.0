492cd709eaec0076854038e342085fb08e046667
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2023-11-12 21:18:59 -0500

    new cache

--- utils/request_handshake.py
+++ utils/request_handshake.py
@@ -10,6 +10,7 @@ Edit Log:
 
 # STANDARD LIBRARY IMPORTS
 from os import getenv
+from json import load, dump
 
 # THIRD PARTY LIBRARY IMPORTS
 from requests import post
@@ -19,16 +20,69 @@ from requests import post
 from utils.firestore_utils import get_base_request
 
 
+def get_authentication_base_request() -> dict:
+    """
+    Gets the authentication base request.
+
+    If authentication is not stale then returns quick,
+    otherwise requests for a new one and updates the cache.
+
+    Returns:
+        dict:   A dictionary representing the authentication base request it
+                contains a username and a token.
+    """
+
+    def read_cache() -> dict:
+        data = {}
+
+        with open(getenv("HANDSHAKE_CACHE_PATH"), encoding="UTF-8") as updated_json:
+            data = load(updated_json)
+
+        return data
+
+    def save_cache(handshake_cache) -> None:
+        with open(
+            file=getenv("HANDSHAKE_CACHE_PATH"), mode="w", encoding="UTF-8"
+        ) as token:
+            dump(handshake_cache, token, default=str)
+
+    try:
+        validation_json = post(
+            url=f"{getenv('TOOLS_URL')}/validateAuthenticationToken",
+            json=read_cache(),
+            timeout=5,
+        ).json()
+
+        if validation_json["ErrorCode"] == 0:
+            return validation_json
+
+    except Exception as exception:  # pylint: disable=broad-exception-caught
+        print("Old cache stale, getting new token", exception)
+
+    base_request = get_base_request()
+
+    authentication_json = post(
+        url=f"{getenv('TOOLS_URL')}/grantAuthenticationToken",
+        json=get_base_request(),
+        timeout=5,
+    ).json()
+
+    handshake_json = {
+        "username": base_request["username"],
+        "token": authentication_json["token"],
+    }
+
+    save_cache(handshake_json)
+
+    return handshake_json
+
+
 class RequestHandshake:
     """
     A class that represents a handshake for making requests.
     """
 
-    base_request = post(
-        url=f"{getenv('TOOLS_URL')}/grantAuthenticationToken",
-        json=get_base_request(),
-        timeout=5,
-    ).json()
+    base_request = get_authentication_base_request()
 
     def __init__(self):
         """
